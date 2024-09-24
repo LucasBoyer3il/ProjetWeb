1ead1ac2609d2b5cffe58885e8f9567b59a9edc6
lucasboyer3il <boyerl@3il.fr>
2024-09-24 16:50:01 +0200

    Création fichier php et génération de balises à partir de ce fichier

---
+++ php/listeItems.php
@@ -0,0 +1,18 @@
+<?php
+echo '
+    <img src="" alt=""/> 
+    <figurecaption>description</figurecaption>
+
+    <img src="" alt=""/> 
+    <p>description</p>
+
+    <img src="" alt=""/> 
+    <p>description</p>
+
+    <img src="" alt=""/> 
+    <p>description</p>
+
+    <img src="" alt=""/> 
+    <p>description</p>
+';
+?>
