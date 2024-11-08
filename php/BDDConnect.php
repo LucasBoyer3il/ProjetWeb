5504ffd5520aa40b71068288994b3da735bfd611
Lucas Boyer <boyerl@3il.fr>
2024-11-08 20:49:13 +0100

    Corrections BDD

--- php/BDDConnect.php
+++ php/BDDConnect.php
@@ -1,17 +1,17 @@
 <?php
-
+/*
 $server = "mysql-l-boyer-3il.alwaysdata.net";
 $db = "l-boyer-3il_jeuxdesociete";
 $user="381270";
 $password="SiteInternet3il";
-
+*/
 //BDD Locale
-/*
+
 $server = "localhost";
 $db = "jeuxdesociete";
 $user="root";
 $password="";
-*/
+
 
 $mysqli = new mysqli($server,$user,$password,$db);
 
